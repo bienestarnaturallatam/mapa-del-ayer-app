06017e170164df72e17912ab1ce472b9f3781334
bienestarnaturallatam <bienestarnaturallatam@gmail.com>
2026-02-11 11:32:17 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#FDFBF7",
+      },
+    },
   },
   plugins: [],
 }
